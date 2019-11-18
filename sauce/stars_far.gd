be63fde7ad8504fdab3ea76d49db87d807278614
joy <joy.sulinom@gmail.com>
2019-11-18 14:54:11 +1300

    2nd commit

---
+++ sauce/stars_far.gd
@@ -0,0 +1,31 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var velocity = Vector2(0,50)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+	pass # Replace with function body.
+
+func get_y():
+	var this_y = position.y + get_offset()
+	return this_y
+	
+func set_y(new_y):
+	position.y = new_y - get_offset()
+	pass
+func get_offset():
+	return offset.y * scale.y
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if get_node("/root/global").time_stopped:
+		return
+	translate(velocity * delta)
+	if get_y() >= get_viewport_rect().size.y:
+		set_y(get_rect().size.y*-1*scale.y)
+	pass
+
+
